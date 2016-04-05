78bdc8c3c7f50ec03645e96ea6e14f1be0383dcc
dorukayhan <dorukayhan@gmail.com>
2016-04-05 17:42:58 -0400

    Symbolically initializes the stuff for the drivetrain

--- src/com/team1389/robot/IOLayout.java
+++ src/com/team1389/robot/IOLayout.java
@@ -1,12 +1,20 @@
 package com.team1389.robot;
 
-import org.strongback.components.TalonSRX;
+//import org.strongback.components.TalonSRX;
+//import edu.wpi.first.wpilibj.CANTalon;
+
+import edu.wpi.first.wpilibj.SpeedController;
 
 public class IOLayout{
 	//put inputs and outputs here, but don't initialize anything. For example:[
-	TalonSRX leftDrive;
-	TalonSRX rightDrive;
+//	TalonSRX leftDrive;
+//	TalonSRX rightDrive;
 	
+	SpeedController[] drivetrainMotors;
+	// You MUST replace the above array with one TalonSRX/CANTalon/Jaguar/CANJaguar/Victor instance per corresponding field in the subsystem
+	// Example with Basic:
+	// CANTalon leftMotor;
+	// CANTalon rightMotor;
 	public IOLayout() {
 		//Initialize motors and sensors here
 	}
