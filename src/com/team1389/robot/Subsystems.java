cd65afb5f25b54dcb9b111fc1430c34708d751cd
dorukayhan <dorukayhan@gmail.com>
2016-09-27 15:33:39 -0400

    Accidentally broke Subsystems.java when cleaning up some mess
    I'm responsible for the mess. By mistake I set up the remote of the
    2017-Robot clone on my PC as the template and then pushed a pre-emptive
    subsystem thinking that it'll appear in the GH repo.

--- src/com/team1389/robot/Subsystems.java
+++ src/com/team1389/robot/Subsystems.java
@@ -1,6 +1,6 @@
 package com.team1389.robot;
 
-import com.team1389.robot.subsystems.*; // Without this we'd write a different import declaration for each subsystem
+//import com.team1389.robot.subsystems.*; // Without this we'd write a different import declaration for each subsystem
 
 /**
  * Put subsystems in this class 
@@ -8,10 +8,8 @@ import com.team1389.robot.subsystems.*; // Without this we'd write a different i
 public class Subsystems {
 	//for example:
 	//public final DriveTrain drive;
-	public final Drivetrain drivetrain;
 	public Subsystems(IOLayout io) {
 		//for example:
 		//drive = new DriveTrain(io.leftMotor, io.rightMotor);
-		drivetrain = new Drivetrain(io.drivetrain_motor1, io.drivetrain_motor2);
 	}
 }
